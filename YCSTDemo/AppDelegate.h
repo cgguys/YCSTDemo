6ddc566a27300c8940c81954a125289b95adf32e
nan gu <407099396@qq.com>
2016-06-07 16:24:49 +0800

    空

---
+++ YCSTDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YCSTDemo
+//
+//  Created by nan gu on 16/6/7.
+//  Copyright © 2016年 nan gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
